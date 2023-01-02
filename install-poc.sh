d1a9d0eb2188f8bd5e2d5b41532fe85d49452d0a
YloXx <jelmer@meetbaar.nl>
2023-01-02 10:52:44 +0100

    Update install-poc.sh

--- install-poc.sh
+++ install-poc.sh
@@ -1,6 +1,25 @@
 #Brewing a DBP Machine installer for clean installs on MacOS
 #/bin/bash -c "$(curl -fsSL https://github.com/YloXx/dbp-brewer/edit/main/install-poc.sh)"
 
+echo "                         &                                                                ,----,                    ,--,       ,--,                           
+                           ,-.----.                              ,--.                    ,/   .`|                 ,---.'|    ,---.'|                           
+    ,---,         ,---,.   \    /  \              ,---,        ,--.'|   .--.--.        ,`   .'  :    ,---,        |   | :    |   | :        ,---,. ,-.----.    
+  .'  .' `\     ,'  .'  \  |   :    \          ,`--.' |    ,--,:  : |  /  /    '.    ;    ;     /   '  .' \       :   : |    :   : |      ,'  .' | \    /  \   
+,---.'     \  ,---.' .' |  |   |  .\ :         |   :  : ,`--.'`|  ' : |  :  /`. /  .'___,/    ,'   /  ;    '.     |   ' :    |   ' :    ,---.'   | ;   :    \  
+|   |  .`\  | |   |  |: |  .   :  |: |         :   |  ' |   :  :  | | ;  |  |--`   |    :     |   :  :       \    ;   ; '    ;   ; '    |   |   .' |   | .\ :  
+:   : |  '  | :   :  :  /  |   |   \ :         |   :  | :   |   \ | : |  :  ;_     ;    |.';  ;   :  |   /\   \   '   | |__  '   | |__  :   :  |-, .   : |: |  
+|   ' '  ;  : :   |    ;   |   : .   /         '   '  ; |   : '  '; |  \  \    `.  `----'  |  |   |  :  ' ;.   :  |   | :.'| |   | :.'| :   |  ;/| |   |  \ :  
+'   | ;  .  | |   :     \  ;   | |`-'          |   |  | '   ' ;.    ;   `----.   \     '   :  ;   |  |  ;/  \   \ '   :    ; '   :    ; |   :   .' |   : .  /  
+|   | :  |  ' |   |   . |  |   | ;             '   :  ; |   | | \   |   __ \  \  |     |   |  '   '  :  | \  \ ,' |   |  ./  |   |  ./  |   |  |-, ;   | |  \  
+'   : | /  ;  '   :  '; |  :   ' |             |   |  ' '   : |  ; .'  /  /`--'  /     '   :  |   |  |  '  '--'   ;   : ;    ;   : ;    '   :  ;/| |   | ;\  \ 
+|   | '` ,/   |   |  | ;   :   : :             '   :  | |   | '`--'   '--'.     /      ;   |.'    |  :  :         |   ,/     |   ,/     |   |    \ :   ' | \.' 
+;   :  .'     |   :   /    |   | :             ;   |.'  '   : |         `--'---'       '---'      |  | ,'         '---'      '---'      |   :   .' :   : :-'   
+|   ,.'       |   | ,'     `---'.|             '---'    ;   |.'                                   `--''                                 |   | ,'   |   |.'     
+'---'         `----'         `---`                      '---'                                                                           `----'     `---'       
+                                                                                                                                                               '
+" 
+
+
 #Get and Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -58,62 +77,6 @@ rm -rf file:///Applications//Applications/Betaflight\ Configurator.app
 #als de dir niet bestaat aanmaken
 mkdir /Applications/M-RDP-config
 nano /Applications/M-RDP-config/eLive-WerkplekOnline.rdp
-
-sudo ex +"r /Applications/M-RDP-config" -cwq test-rdp-output.rdp <<-EOF
-armpath:s:
-targetisaadjoined:i:0
-hubdiscoverygeourl:s:
-redirected video capture encoding quality:i:0
-camerastoredirect:s:
-gatewaybrokeringtype:i:0
-use redirection server name:i:0
-alternate shell:s:
-disable themes:i:0
-disable cursor setting:i:1
-remoteapplicationname:s:
-resourceprovider:s:
-disable menu anims:i:1
-remoteapplicationcmdline:s:
-promptcredentialonce:i:0
-gatewaycertificatelogonauthority:s:
-audiocapturemode:i:0
-prompt for credentials on client:i:0
-gatewayhostname:s:desktop.elive.nl
-remoteapplicationprogram:s:
-gatewayusagemethod:i:2
-screen mode id:i:2
-use multimon:i:0
-authentication level:i:2
-desktopwidth:i:0
-desktopheight:i:0
-redirectsmartcards:i:0
-redirectclipboard:i:1
-forcehidpioptimizations:i:0
-full address:s:eli-ts-dbp.elive.nl:3389
-drivestoredirect:s:*
-loadbalanceinfo:s:
-networkautodetect:i:1
-enablecredsspsupport:i:1
-redirectprinters:i:1
-autoreconnection enabled:i:1
-session bpp:i:32
-administrative session:i:0
-audiomode:i:0
-bandwidthautodetect:i:1
-authoring tool:s:
-connection type:i:7
-remoteapplicationmode:i:0
-disable full window drag:i:0
-gatewayusername:s:email@debreed.nl
-shell working directory:s:
-wvd endpoint pool:s:
-remoteapplicationappid:s:
-username:s:email@debreed.nl
-allow font smoothing:i:1
-connect to console:i:0
-disable wallpaper:i:0
-gatewayaccesstoken:s:
-EOF
 
 
 #Checkup for the total installed packages on the machine
