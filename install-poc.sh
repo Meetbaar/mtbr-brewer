bf1828741a9f82c6083c490e6045aa874467002a
YloXx <jelmer@meetbaar.nl>
2023-01-02 14:39:49 +0100

    Update install-poc.sh

--- install-poc.sh
+++ install-poc.sh
@@ -47,18 +47,44 @@ else
 fi
 
 
-#delete applications
-#delete garageband
+# Delete obsolete applications
 #rm -rf file:///Applications/GarageBand.app
-rm -rf file:///Applications//Applications/Betaflight\ Configurator.app 
+sudo rm –rf /System/Applications/Betaflight Configurator.app
+sudo rm –rf /System/Applications/Stocks.app
 
 
-#config the remote desktop app with setup except credentials
+# config the remote desktop app with setup except credentials
 
 #als de dir niet bestaat aanmaken
 mkdir /Applications/M-RDP-config
 nano /Applications/M-RDP-config/eLive-WerkplekOnline.rdp
 
 
-#Checkup for the total installed packages on the machine
+# Dockinstall make shure it is bash and sudo 
+LOGGED_USER=`stat -f%Su /dev/console` 
+sudo su $LOGGED_USER -c 'defaults delete com.apple.dock persistent-apps' 
+
+dock_item() { 
+    printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>%s</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>', "$1" 
+} 
+
+chrome=$(dock_item /System/Applications/Google Chrome.app) 
+docs=$(dock_item /System/Applications/Google Docs.app)
+sheets=$(dock_item /System/Applications/Google Sheets.app) 
+slides=$(dock_item /System/Applications/Google Slides.app) 
+drive=$(dock_item /System/Applications/Google Drive.app) 
+teamviewer=$(dock_item /System/Applications/TeamViewer.app)
+msrdp=$(dock_item /System/Applications/TeamViewer.app) 
+spotify=$(dock_item /System/Applications/Spotify.app) 
+
+sudo su $LOGGED_USER -c "defaults write com.apple.dock persistent-apps -array '$chome' '$docs' '$sheets' '$slides' '$drive' '$teamviewer' '$msrdp' '$spotify'" 
+killall Dock 
+
+
+# Checkup for the total installed packages on the machine
 brew doctor
+
+# turnon fireall for everyone
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1 
+
+
