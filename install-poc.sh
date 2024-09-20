de258b0261e18fb4126548a94cf709cd4bc0003a
YloXx <jelmer@meetbaar.nl>
2024-09-20 09:53:39 +0200

    Update install-poc.sh

--- install-poc.sh
+++ install-poc.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-echo "dbp-brewer ¯\\\_(ツ)_/¯ V1.1.230214 \r\r"
+echo "mtbr-brewer ¯\\\_(ツ)_/¯ V1.1.230214 \r\r"
 echo ""
-echo "You are going to install the dbp-brewer for the following system!"
+echo "You are going to install the mtbr-brewer for the following system!"
 array=$( system_profiler SPSoftwareDataType )
 
 for i in "${array[@]}"; do
@@ -53,8 +53,8 @@ echo "Fill in Macbook follownumber"
 read iterationNumber
 
 if [ "$computerName" != "" ] ;then
-	$(sudo scutil --set ComputerName "DBP-MBM1P-$(date +%y)"$iterationNumber"-"$computerName)
-	$(sudo scutil --set LocalHostName "DBP-MBM1P-$(date +%y)"$iterationNumber"-"$computerName)
+	$(sudo scutil --set ComputerName "PWRSTAFF-MBP-$(date +%y)"$iterationNumber"-"$computerName)
+	$(sudo scutil --set LocalHostName "PWRSTAFF-MBP-$(date +%y)"$iterationNumber"-"$computerName)
 fi
 
 echo "Do you want to add an default Admin account, select (y/n)?"
@@ -83,12 +83,12 @@ if [ "$default_admin" != "${default_admin#[Yy]}" ] ; then
 	echo "Admin succesfully created"
 fi
 
-echo  "Do you want to install default DBP applications $email_employee select (y/n)?"
+echo  "Do you want to install default PWRSTAFF / HSL / GRNTLY applications $email_employee select (y/n)?"
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ; then
     echo "Hold your seat..! \n"
-    echo -ne 'Prepare: Google Chrome, Drive, Chat, MS Remote desktop, teamviewer, Spotify and many more...\n'
+    echo -ne 'Prepare: Google Chrome, Drive, Chat, teamviewer, Spotify and many more...\n'
     sleep 2
     echo -ne '#                         (2%)\r'
     sleep 1
@@ -99,7 +99,6 @@ if [ "$answer" != "${answer#[Yy]}" ] ; then
     brew install --cask google-chrome
     brew install --cask google-drive
     brew install --cask google-chat
-    brew install --cask microsoft-remote-desktop
     brew install --cask teamviewer
     brew install --cask spotify
     brew install --cask vlc
@@ -108,84 +107,8 @@ if [ "$answer" != "${answer#[Yy]}" ] ; then
     sleep 1
     echo -ne '#######################   (100%)\r'
     echo -ne '\n'
-    rdpconfigfile=/Applications/M-RDP-config/eLive-WerkplekOnline.rdp
-    if [ -f "$rdpconfigfile" ]; then
-        echo "RDP config exists."
-    else 
-        echo "RDP config does not exist."
-        echo "So lets create some configs!"
-        echo -ne '#################         (81%)\r'
-        sleep 1
-        echo -ne '#######################   (100%)\r'
-        echo -ne '\n'
-        mkdir /Applications/M-RDP-config && touch $rdpconfigfile
-        
-        cat <<< "smart sizing:i:0
-armpath:s:
-targetisaadjoined:i:0
-hubdiscoverygeourl:s:
-redirected video capture encoding quality:i:0
-camerastoredirect:s:
-gatewaybrokeringtype:i:0
-use redirection server name:i:0
-alternate shell:s:
-disable themes:i:0
-geo:s:
-disable cursor setting:i:1
-remoteapplicationname:s:
-resourceprovider:s:
-disable menu anims:i:1
-remoteapplicationcmdline:s:
-promptcredentialonce:i:0
-gatewaycertificatelogonauthority:s:
-audiocapturemode:i:0
-prompt for credentials on client:i:0
-gatewayhostname:s:desktop.elive.nl
-remoteapplicationprogram:s:
-gatewayusagemethod:i:2
-screen mode id:i:2
-use multimon:i:0
-authentication level:i:2
-desktopwidth:i:0
-desktopheight:i:0
-redirectsmartcards:i:0
-redirectclipboard:i:1
-forcehidpioptimizations:i:0
-full address:s:eli-ts-dbp.elive.nl:3389
-drivestoredirect:s:*
-loadbalanceinfo:s:
-networkautodetect:i:1
-enablecredsspsupport:i:1
-redirectprinters:i:1
-autoreconnection enabled:i:1
-session bpp:i:32
-administrative session:i:0
-audiomode:i:0
-bandwidthautodetect:i:1
-authoring tool:s:
-connection type:i:7
-remoteapplicationmode:i:0
-disable full window drag:i:0
-gatewayusername:s:$email_employee
-dynamic resolution:i:0
-shell working directory:s:
-wvd endpoint pool:s:
-remoteapplicationappid:s:
-username:s:$email_employee
-allow font smoothing:i:1
-connect to console:i:0
-disable wallpaper:i:0
-gatewayaccesstoken:s:" > $rdpconfigfile
-        cat $rdpconfigfile
-
-    fi
-
-else
-    echo "Clean installation without apps!"
-fi
 
 sleep 1
-
 
 echo  "Do you want to install dockitems? select (y/n)"
 read dock_items
